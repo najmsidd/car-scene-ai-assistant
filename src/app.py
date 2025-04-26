c3ab82105687ff713763f7c8fa480d3daf34e4a9
najmsidd <najmsiddiqui007@gmail.com>
2025-04-26 15:27:41 +0530

    Added video upload and video analysis functionality in Streamlit app

--- src/app.py
+++ src/app.py
@@ -3,56 +3,78 @@ from detect import detect_image
 from gpt_response import build_prompt, gpt_response, build_driver_prompt
 import os
 from PIL import Image
+from video_pipeline import detect_video
 
 st.title("Car scene ai assistant")
 
-uploaded_file = st.file_uploader("Upload an image of the traffic", type=["jpg", "jpeg", "png"])
+os.makedirs("images", exist_ok=True)
+os.makedirs("videos", exist_ok=True)
+os.makedirs("detections", exist_ok=True)
 
-if uploaded_file is not None:
+option = st.selectbox("Choose input type",("Image", "Video")) 
 
-    image_path = os.path.join("images", uploaded_file.name)
+if option == "Image":
+    uploaded_file = st.file_uploader("Upload an image of the traffic", type=["jpg", "jpeg", "png"])
 
-    with open(image_path, "wb") as f:
-        f.write(uploaded_file.getbuffer())
+    if uploaded_file is not None:
 
-    st.image(image_path, caption="Uploaded traffic image", use_column_width=True)
+        image_path = os.path.join("images", uploaded_file.name)
 
-    if st.button("Analyze image"):
+        with open(image_path, "wb") as f:
+            f.write(uploaded_file.getbuffer())
 
-        object_counts, output_file_path= detect_image(image_path)
+        st.image(image_path, caption="Uploaded traffic image", use_column_width=True)
 
-        st.image(output_file_path, caption="Analyzed Image", use_column_width=True)
+        if st.button("Analyze image"):
 
-        st.subheader("Detected Objects:")
-        st.write(object_counts)
+            object_counts, output_file_path= detect_image(image_path)
 
-        builded_prompt = build_prompt(object_counts)
-        st.subheader("Scene Description Prompt:")
-        st.code(builded_prompt)
+            st.image(output_file_path, caption="Analyzed Image", use_column_width=True)
 
-        gpt_generated_response = gpt_response(builded_prompt)
-        st.subheader("GPT Scene response:")
-        st.write(gpt_generated_response)
+            st.subheader("Detected Objects:")
+            st.write(object_counts)
 
-        driver_prompt = build_driver_prompt(object_counts)
-        st.subheader("Driver Recommendations Prompt:")
-        st.code(driver_prompt)
+            builded_prompt = build_prompt(object_counts)
+            st.subheader("Scene Description Prompt:")
+            st.code(builded_prompt)
 
-        gpt_driver_response = gpt_response(driver_prompt)
-        st.subheader("GPT Driver Recommendations")
-        st.write(gpt_driver_response)
+            gpt_generated_response = gpt_response(builded_prompt)
+            st.subheader("GPT Scene response:")
+            st.write(gpt_generated_response)
 
-        with open(output_file_path, "rb") as img_file:
-            st.download_button(label="Download analyzed image", data=img_file, file_name=output_file_path)
+            driver_prompt = build_driver_prompt(object_counts)
+            st.subheader("Driver Recommendations Prompt:")
+            st.code(driver_prompt)
 
+            gpt_driver_response = gpt_response(driver_prompt)
+            st.subheader("GPT Driver Recommendations")
+            st.write(gpt_driver_response)
 
+            with open(output_file_path, "rb") as img_file:
+                st.download_button(label="Download analyzed image", data=img_file, file_name=output_file_path)
 
+elif option == "Video":
+    uploaded_video = st.file_uploader("Upload a video of the traffic", type=["mp4","avi","mov"])
 
+    if uploaded_video is not None:
+        video_path = os.path.join("videos", uploaded_video.name)
 
-    
+        with open(video_path, "wb") as f:
+            f.write(uploaded_video.getbuffer())
 
+        st.video(video_path)
 
+        if st.button("Analyze video"):
 
+            output_video_path = os.path.join("detections", f"detected_{uploaded_video.name}")
 
+            detect_video(video_path,output_video_path)
 
+            st.success("Video processed successfully!")
 
+            with open(output_video_path, "rb") as vid_file:
+                video_bytes = vid_file.read()
+
+            st.video(video_bytes)
+
+            st.download_button(label="Download analyzed video", data=video_bytes, file_name=output_video_path)
--- src/video_pipeline.py
+++ src/video_pipeline.py
@@ -2,35 +2,33 @@ from ultralytics import YOLO
 import cv2
 import os
 
-VIDEO_PATH = "videos/sample_video.mp4"
-OUTPUT_PATH = "detections/sample_video_detected.mp4"
-
 model = YOLO("yolov8n.pt")
 
-cap = cv2.VideoCapture(VIDEO_PATH)
+def detect_video(input_path, output_path):
+    cap = cv2.VideoCapture(input_path)
 
-fps = cap.get(cv2.CAP_PROP_FPS)
-width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-out = cv2.VideoWriter(OUTPUT_PATH, fourcc, fps, (width, height))
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter(output_path, fourcc, fps, (width, height))
 
-print("Processing video...")
+    print("Processing video...")
 
-while cap.isOpened():
-    ret, frame = cap.read()
-    if not ret:
-        break
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
 
-    results = model(frame, verbose = False)
+        results = model(frame, verbose = False)
 
-    annotated_frame = results[0].plot()
+        annotated_frame = results[0].plot()
 
-    out.write(annotated_frame)
+        out.write(annotated_frame)
 
-cap.release()
-out.release()
+    cap.release()
+    out.release()
 
-print(f"Save detected file to {OUTPUT_PATH}")
+    print(f"Save detected file to {output_path}")
 
