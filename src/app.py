e103df958a133dfd40e9bcc2572511b7a0067523
najmsidd <najmsiddiqui007@gmail.com>
2025-04-27 19:16:16 +0530

    Enhanced video analysis with object tracking and better counting using DeepSort

--- src/app.py
+++ src/app.py
@@ -68,7 +68,7 @@ elif option == "Video":
 
             output_video_path = os.path.join("detections", f"detected_{uploaded_video.name}")
 
-            detect_video(video_path,output_video_path)
+            object_counts = detect_video(video_path, output_video_path)
 
             st.success("Video processed successfully!")
 
@@ -76,5 +76,24 @@ elif option == "Video":
                 video_bytes = vid_file.read()
 
             st.video(video_bytes)
+
+            st.subheader("Detected objects in video:")
+            st.write(object_counts)
+
+            builded_prompt = build_prompt(object_counts)
+            st.subheader("Scene description prompt(video):")
+            st.code(builded_prompt)
+
+            gpt_generated_response = gpt_response(builded_prompt)
+            st.subheader("GPT Scene response (video)")
+            st.write(gpt_generated_response)
+
+            driver_prompt = build_driver_prompt(object_counts)
+            st.subheader("GPT Driver recommendations (video")
+            st.code(driver_prompt)
+
+            gpt_driver_response = gpt_response(driver_prompt)
+            st.subheader("GPT Driver recommendations (video)")
+            st.write(gpt_driver_response)
 
             st.download_button(label="Download analyzed video", data=video_bytes, file_name=output_video_path)
--- src/video_pipeline.py
+++ src/video_pipeline.py
@@ -1,10 +1,13 @@
 from ultralytics import YOLO
 import cv2
 import os
+from collections import defaultdict
+from deep_sort_realtime.deepsort_tracker import DeepSort
 
 model = YOLO("yolov8n.pt")
+tracker = DeepSort(max_age=30)
 
-def detect_video(input_path, output_path):
+def detect_video(input_path, output_path, frame_interval = 30):
     cap = cv2.VideoCapture(input_path)
 
     fps = cap.get(cv2.CAP_PROP_FPS)
@@ -16,19 +19,54 @@ def detect_video(input_path, output_path):
 
     print("Processing video...")
 
+    frame_count = 0
+    final_counts = defaultdict(int)
+    counted_ids = set()
+
     while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
             break
 
         results = model(frame, verbose = False)
+        detections = results[0]
 
-        annotated_frame = results[0].plot()
+        det_list = []
+        for box in detections.boxes:
+            x1, y1, x2, y2 = box.xyxy[0].tolist()
+            conf = box.conf[0].item()
+            cls = int(box.cls[0].item())
+            det_list.append(([x1, y1, x2-x1, y2-y1], conf, cls))
 
-        out.write(annotated_frame)
+        tracks = tracker.update_tracks(det_list, frame= frame)
 
+        for track in tracks:
+
+            if not track.is_confirmed():
+                continue
+
+            track_id = track.track_id
+            cls = track.get_det_class()
+            label = detections.names[int(cls)]
+
+            if track_id not in counted_ids:
+                final_counts[label] += 1
+                counted_ids.add(track_id)
+
+            l,t,w,h = track.to_ltrb()
+            r, b = l+w, t+h
+            cv2.rectangle(frame, (int(l), int(t), int(r), int(b)), (0,255,0), 2)
+            cv2.putText(frame, f'{label} ID:{track_id}',(int(l), int(t)-10),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0), 2)
+
+        out.write(frame)
+        frame_count += 1
+        
     cap.release()
     out.release()
 
-    print(f"Save detected file to {output_path}")
+    print(f"Saved detected video to {output_path}")
+    return dict(final_counts)
+
+        
+
 
